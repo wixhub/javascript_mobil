c57efc9c97ccf7405af91f3b16fa5674e388a657
Rub1in <66561004+Rub1in@users.noreply.github.com>
2020-11-06 07:50:33 +0100

    change html

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Javascript mobile Apps
-1. <a href="https://wixhub.github.io/javascript_mobile/compass.html" target="_blank" >Compass</a> Read sensors from mobile devices 
+1. <a href="https://wixhub.github.io/javascript_mobile/compass.html" target="_blank" >Compass</a> - read sensors from mobile devices 
--- js/compass.js
+++ js/compass.js
@@ -1,4 +1,7 @@
 function ausrichtungAuslesen(ereignis) {
+  if (ereignis.gamma != null) {
+    document.getElementById('datenMessage').innerHTML = "Read sensors from mobile devices";
+    document.getElementById('datenABG').style.visibility = "visible";
     var winkelAlpha = Math.round(ereignis.alpha * 100) / 100; 
     var winkelBeta = Math.round(ereignis.beta * 100) / 100;
     var winkelGamma = Math.round(ereignis.gamma * 100) / 100;
@@ -6,6 +9,7 @@ function ausrichtungAuslesen(ereignis) {
     document.getElementById('datenBeta').innerHTML = winkelBeta;
     document.getElementById('datenGamma').innerHTML = winkelGamma;
     document.getElementById("compass").style.transform = "rotate(" + winkelAlpha + "deg)";
+  }
 }
 window.onload = function() {
   window.addEventListener("deviceorientation", ausrichtungAuslesen, false);
